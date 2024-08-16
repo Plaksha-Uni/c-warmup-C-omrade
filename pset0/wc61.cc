96f523bdd62d07b5f8b105b9dc8bc2767d8adc66
C-omrade <alone.silent.hunter@gmail.com>
2024-08-17 00:30:41 +0530

    Finally done with the part 4 of the assignment

---
+++ pset0/wc61.cc
@@ -0,0 +1,29 @@
+// code for counting words and lines
+#include<cstdlib>
+#include<cstdio>
+#include<cstring>
+#include<cctype>
+int main(){
+    int count_lines = 0;
+    int count_bytes = 0;
+    int count_words = 0;
+    char prev = 'a';
+    while(true){
+        char c = fgetc(stdin);
+        if(c==EOF){
+            if(!isspace(prev)){
+                count_words++;
+            }
+            break;}
+        count_bytes++;
+        if(isspace(c) && prev!=c){
+            count_words++;
+        }
+        if(c=='\n'){
+            count_lines++;
+        }
+        prev = c;
+    }
+    fprintf(stdout,"%d %d %d\n",count_lines,count_words,count_bytes);
+    exit(0);
+}
