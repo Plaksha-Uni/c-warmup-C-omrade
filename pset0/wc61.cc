bcfb9f507ceaeabb29031fbb28fd07cefec45e07
C-omrade <alone.silent.hunter@gmail.com>
2024-08-17 01:48:50 +0530

    Corrected the 4th part of the code

--- pset0/wc61.cc
+++ pset0/wc61.cc
@@ -7,17 +7,19 @@ int main(){
     int count_lines = 0;
     int count_bytes = 0;
     int count_words = 0;
-    char prev = 'a';
+    char prev = ' ';
     while(true){
+        
         char c = fgetc(stdin);
         if(c==EOF){
-            if(!isspace(prev)){
+            if(!isspace(prev) && prev!='\0'){
                 count_words++;
             }
             break;}
         count_bytes++;
-        if(isspace(c) && prev!=c){
+        if(isspace(c) && prev!=c && c!='\n'){
             count_words++;
+    
         }
         if(c=='\n'){
             count_lines++;
